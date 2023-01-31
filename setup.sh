2ec050991e344001fd42b8e6631327cdd6050863
zztaki <50295684+zztaki@users.noreply.github.com>
2023-01-31 16:27:34 +0800

    Update setup.sh
    echo 启动转义功能，输出高亮

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@ echo -e "\033[33mStart checking whether the Go development environment is instal
 	go version
     # shellcheck disable=SC2181
     if [ $? -eq  0 ]; then
-        echo "\033[32m[Success] The Go development environment has been detected!\033[0m"
+        echo -e "\033[32m[Success] The Go development environment has been detected!\033[0m"
     else
-    	echo "\033[31m[Error] The current device does not detect the GO development environment, please refer to the manual for installation.\033[0m"
+    	echo -e "\033[31m[Error] The current device does not detect the GO development environment, please refer to the manual for installation.\033[0m"
     	exit
     fi
 
@@ -12,9 +12,9 @@ echo -e "\033[33mStart checking whether the docker application is installed loca
 	docker -v
     # shellcheck disable=SC2181
     if [ $? -eq  0 ]; then
-        echo "\033[32m[Success] The docker application has been detected!\033[0m"
+        echo -e "\033[32m[Success] The docker application has been detected!\033[0m"
     else
-    	echo "\033[31m[Error] The current device does not detect the docker application, please refer to the manual for installation.\033[0m"
+    	echo -e "\033[31m[Error] The current device does not detect the docker application, please refer to the manual for installation.\033[0m"
       exit
     fi
 
@@ -22,19 +22,19 @@ echo -e "\033[33mStart checking whether the docker-compose application is instal
 	docker-compose version
     # shellcheck disable=SC2181
     if [ $? -eq  0 ]; then
-        echo "\033[32m[Success] The docker-compose application has been detected!\033[0m"
+        echo -e "\033[32m[Success] The docker-compose application has been detected!\033[0m"
     else
-    	echo "\033[31m[Error] The current device does not detect the docker-compose application, please refer to the manual for installation.\033[0m"
+    	echo -e "\033[31m[Error] The current device does not detect the docker-compose application, please refer to the manual for installation.\033[0m"
       exit
     fi
 
 echo -e "\033[33mStart build test...... \033[0m"
   go build -o young_engine
     if [ $? -eq  0 ]; then
-        echo "\033[32m[Success] build success!\033[0m"
+        echo -e "\033[32m[Success] build success!\033[0m"
         rm ./young_engine
     else
-    	echo "\033[31m[Error] build failed, Please resolve the issue based on the error message\033[0m"
+    	echo -e "\033[31m[Error] build failed, Please resolve the issue based on the error message\033[0m"
       exit
     fi
 
